3a4b81cdf5e1ba1c32421d81b2354ec9f645f856
natanmaiagt <natanmaia_17@hotmail.com>
2020-01-06 22:11:57 -0300

    Implementação das classes de controle do response body, tratamento de exceções e erros http request

--- src/main/java/br/com/natanmaia/MathController.java
+++ src/main/java/br/com/natanmaia/MathController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.natanmaia.exception.UnsuportedMathOperationException;
+
 @RestController
 public class MathController {
 	
@@ -13,7 +15,7 @@ public class MathController {
 			@PathVariable("numberOne") String numberOne, 
 			@PathVariable("numberTwo") String numberTwo) throws Exception {
 		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new Exception();
+			throw new UnsuportedMathOperationException("Por favor, defina um valor numérico!");
 		}
 		Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo); 
 		return sum;
