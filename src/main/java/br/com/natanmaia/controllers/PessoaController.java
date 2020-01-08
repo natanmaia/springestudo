09ef1422fecee2629994a7d62c2a116e9cc4a2b6
natanmaiagt <natanmaia_17@hotmail.com>
2020-01-07 22:01:39 -0300

    criação do metodo buscarTodos

--- src/main/java/br/com/natanmaia/controllers/PessoaController.java
+++ src/main/java/br/com/natanmaia/controllers/PessoaController.java
@@ -1,5 +1,7 @@
 package br.com.natanmaia.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +19,21 @@ public class PessoaController {
 	@Autowired
 	private PessoaService pessoaService;
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/{id}", 
+			method = RequestMethod.GET, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Pessoa buscarPorId(
 			@PathVariable("id") String id) {
 		
 		return pessoaService.buscarPorId(id);
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Pessoa> buscarTodos() {
+		
+		return pessoaService.buscarTodos();
 		
 	}
 }
--- src/main/java/br/com/natanmaia/services/PessoaService.java
+++ src/main/java/br/com/natanmaia/services/PessoaService.java
@@ -1,5 +1,7 @@
 package br.com.natanmaia.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -18,6 +20,25 @@ public class PessoaService {
 		pessoa.setSobrenome("Maia");
 		pessoa.setEndereco("Horizonte, Ceará");
 		pessoa.setGenero("Masculino");
+		return pessoa;
+	}
+	
+	public List<Pessoa> buscarTodos() {
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		for (int i = 0; i < 8; i++) {
+			Pessoa pessoa = mockPessoa(i);
+			pessoas.add(pessoa);
+		}
+		return pessoas;
+	}
+
+	private Pessoa mockPessoa(int i) {
+		Pessoa pessoa = new Pessoa();
+		pessoa.setId(counter.incrementAndGet());
+		pessoa.setNome("Nome " + i);
+		pessoa.setSobrenome("Sobrenome " + 1);
+		pessoa.setEndereco("Endereço " + 1);
+		pessoa.setGenero("Genero");
 		return pessoa;
 	}
 }
