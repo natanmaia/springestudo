f162ad1802e86a361fa0fcab98054695665a7976
natanmaia <natanmaia_17@hotmail.com>
2021-01-30 10:31:41 -0300

    ajustes para swagger

--- src/main/java/br/com/natanmaia/config/SwaggerConfig.java
+++ src/main/java/br/com/natanmaia/config/SwaggerConfig.java
@@ -1,15 +1,15 @@
 package br.com.natanmaia.config;
 
-import io.swagger.annotations.SwaggerDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@SwaggerDefinition
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
