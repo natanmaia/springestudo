c8ef34b5aa517027122070c69f6e96efa3c6741c
natanmaia <natanmaia_17@hotmail.com>
2021-01-31 13:30:38 -0300

    implementação do VO para response do Upload

---
+++ src/main/java/br/com/natanmaia/data/vo/UploadFileResponseVO.java
@@ -0,0 +1,71 @@
+package br.com.natanmaia.data.vo;
+
+import java.io.Serializable;
+
+public class UploadFileResponseVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String fileName;
+    private String fileDownloadUri;
+    private String fileType;
+    private String size;
+
+    public UploadFileResponseVO(){
+
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileDownloadUri() {
+        return fileDownloadUri;
+    }
+
+    public void setFileDownloadUri(String fileDownloadUri) {
+        this.fileDownloadUri = fileDownloadUri;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UploadFileResponseVO)) return false;
+
+        UploadFileResponseVO that = (UploadFileResponseVO) o;
+
+        if (!getFileName().equals(that.getFileName())) return false;
+        if (!getFileDownloadUri().equals(that.getFileDownloadUri())) return false;
+        if (!getFileType().equals(that.getFileType())) return false;
+        return getSize().equals(that.getSize());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getFileName().hashCode();
+        result = 31 * result + getFileDownloadUri().hashCode();
+        result = 31 * result + getFileType().hashCode();
+        result = 31 * result + getSize().hashCode();
+        return result;
+    }
+}
