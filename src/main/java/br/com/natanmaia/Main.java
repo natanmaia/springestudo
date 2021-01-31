d120f21cf6d5ecfda321ae267dd27a114992a03d
natanmaia <natanmaia_17@hotmail.com>
2021-01-31 13:20:42 -0300

    implementação da configuração para upload de arquivos

--- src/main/java/br/com/natanmaia/Main.java
+++ src/main/java/br/com/natanmaia/Main.java
@@ -1,12 +1,15 @@
 package br.com.natanmaia;
 
+import br.com.natanmaia.config.FileStorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileStorageConfig.class})
 @EnableAutoConfiguration
 @ComponentScan
 public class Main {
