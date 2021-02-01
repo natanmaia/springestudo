dd71f7d401737553b345f44293edbedad71a942b
natanmaia <natanmaia_17@hotmail.com>
2021-02-01 10:14:47 -0300

    implementação de download de arquivos

--- src/main/java/br/com/natanmaia/controllers/FileController.java
+++ src/main/java/br/com/natanmaia/controllers/FileController.java
@@ -3,14 +3,18 @@ package br.com.natanmaia.controllers;
 import br.com.natanmaia.data.vo.UploadFileResponseVO;
 import br.com.natanmaia.services.FileStorageService;
 import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,11 +24,13 @@ import java.util.stream.Collectors;
 @RequestMapping("api/v1/file")
 public class FileController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileController.class);
+
     @Autowired
     private FileStorageService fileStorageService;
 
     @PostMapping("/uploadFile")
-    public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file){
+    public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file) {
 
         String fileName = fileStorageService.storeFile(file);
 
@@ -37,10 +43,33 @@ public class FileController {
     }
 
     @PostMapping("/uploadMultiFiles")
-    public List<UploadFileResponseVO> uploadMultiFile(@RequestParam("files") MultipartFile[] files){
+    public List<UploadFileResponseVO> uploadMultiFile(@RequestParam("files") MultipartFile[] files) {
         return Arrays.asList(files)
                 .stream()
                 .map(file -> uploadFile(file))
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        String contentType = null;
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            LOGGER.info("Could not determine file type!");
+        }
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
--- src/main/java/br/com/natanmaia/services/FileStorageService.java
+++ src/main/java/br/com/natanmaia/services/FileStorageService.java
@@ -2,7 +2,10 @@ package br.com.natanmaia.services;
 
 import br.com.natanmaia.config.FileStorageConfig;
 import br.com.natanmaia.exception.FileStorageException;
+import br.com.natanmaia.exception.MyFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,15 +27,15 @@ public class FileStorageService {
 
         try {
             Files.createDirectories(this.fileStorageLocation);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new FileStorageException("Could not create the directory where the upload files will be stored!", e);
         }
     }
 
-    public String storeFile(MultipartFile multipartFile){
+    public String storeFile(MultipartFile multipartFile) {
         String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
         try {
-            if (fileName.contains("..")){
+            if (fileName.contains("..")) {
                 throw new FileStorageException("Sorry! FileName contains invalid path sequence. FileName: " + fileName);
             }
 
@@ -40,8 +43,23 @@ public class FileStorageService {
 
             Files.copy(multipartFile.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
             return fileName;
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new FileStorageException("Could not store file " + fileName + ". Please, try again!", e);
+        }
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new MyFileNotFoundException("File not Found! " + fileName);
+            }
+        } catch (Exception e) {
+            throw new MyFileNotFoundException("File not Found! " + fileName, e);
         }
     }
 }
